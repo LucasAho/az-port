1bf27b76939be840e200a51f473f604ba0646146
Lucas Aho <aholucascode@gmail.com>
2021-08-11 13:48:22 -0600

    I've probably already tried this

--- server.js
+++ server.js
@@ -8,17 +8,16 @@ const path = require("path");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static("./client/az-react/build/"));
+
+app.use(express.static("./client/az-react/build"));
 
 const words_route = require("./server/routes/words-route");
 words_route(app);
 
-if (process.env.NODE_ENV === 'production') {
-    app.get('*', (req, res) => {
-        res.sendFile('index.html', {root: __dirname + '/client/az-react/build/'});
-    });
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'az-react', 'build', "index.html"));
+});
 
 
-const {PORT} = process.env;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Broadcasting from port ${PORT}`));
